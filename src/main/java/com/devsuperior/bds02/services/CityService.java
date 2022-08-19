f8c8358eed394ec6cfd6938d33286bcc79e2d2dc
maicon.lopes <maicon.lopes@agi.com.br>
2022-08-19 11:11:23 -0300

    final

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,54 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CityService {
+
+    @Autowired
+    public CityRepository cityRepository;
+
+    public void delete(Long id) {
+        try {
+            cityRepository.deleteById(id);
+
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id Not Found: " + id);
+
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
+
+    public List<CityDTO> findAll() {
+
+        List<City> list = cityRepository.findAll(Sort.by("name"));
+        return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CityDTO insert(CityDTO dto) {
+        City entity = new City();
+        entity.setName(dto.getName());
+        entity = cityRepository.save(entity);
+        return new CityDTO(entity);
+    }
+}
+
+
+
+
+
